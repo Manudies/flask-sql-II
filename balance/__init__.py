e24c5924517a8cd55e3cdbebcc733a6b976538c9
Tony G. Bolaño <tony@tonygb.com>
2023-10-03 20:40:13 +0200

    feat: preparamos la vista y template de eliminar

--- balance/__init__.py
+++ balance/__init__.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import Flask
 
+RUTA = os.path.join('balance', 'data', 'balance.db')
 
 app = Flask(__name__)
--- balance/views.py
+++ balance/views.py
@@ -1,12 +1,22 @@
 from flask import render_template
 
-from . import app
+from . import RUTA, app
 from .models import DBManager
 
 
 @app.route('/')
 def home():
-    db = DBManager('balance/data/balance.db')
+    db = DBManager(RUTA)
     sql = 'SELECT id, fecha, concepto, tipo, cantidad FROM movimientos'
     movimientos = db.consultaSQL(sql)
     return render_template('inicio.html', movs=movimientos)
+
+
+# - Función borrar  -- DONE
+# - Operar con la BD
+# - Botón de borrado en cada movimiento
+# - Plantilla con el resultado -- DONE
+
+@app.route('/borrar')
+def eliminar():
+    return render_template('borrado.html', resultado=False)
