18aebe7fc4f4a9ac4e25b8331b5557e7a6ed9c85
Tony G. Bolaño <tony@tonygb.com>
2023-10-03 22:55:33 +0200

    feat: probamos a cargar datos de un movimiento en el formulario

--- balance/views.py
+++ balance/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from flask import render_template, request
 
 from . import RUTA, app
@@ -28,6 +30,13 @@ def eliminar(id):
 @app.route('/editar/<int:id>')
 def actualizar(id):
     if request.method == 'GET':
-        formulario = MovimientoForm()
+        movimiento = {
+            'id': 55,
+            'fecha': date.fromisoformat('2023-10-03'),
+            'concepto': 'Curso de formularios en Python',
+            'tipo': 'I',
+            'cantidad': 55.955874587
+        }
+        formulario = MovimientoForm(data=movimiento)
         return render_template('form_movimiento.html', form=formulario)
     return f'TODO: tratar el método POST para actualizar el movimiento {id}'
